feb820c4ac6d27566b6cc477f29940d771f4db5a
sahil <sahil.98@hotmail.com>
2023-11-24 10:03:12 +0000

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	int row1[3] = {1, 2, 3};
+	int row1[3] = {4, 5, 6};
+	int row1[3] = { 7, 8, 9 };
+
+
+
+
+}
