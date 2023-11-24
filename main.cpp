80f79e86a92af6ce8f7de6f10ace395faf5e50f9
sahil <sahil.98@hotmail.com>
2023-11-24 12:04:16 +0000

    Created single int arrays containing three values to visualise the tictactoe board.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,25 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-#include <vector>
+using namespace std;
+
+
+void displayBoard(){
+
+	int row1[3] = {1, 2, 3};
+	int row2[3] = {4, 5, 6};
+	int row3[3] = { 7, 8, 9 };
+
+	// int array[3][3]
+
+	cout << "\n\t" << row1[0] << "|" << row1[1] << "|" << row1[2] << "|";
+	cout << "\n\t" << row2[0] << "|" << row2[1] << "|" << row2[2] << "|";
+	cout << "\n\t" << row3[0] << "|" << row3[1] << "|" << row3[2] << "|"<<endl;
+}
 
 int main()
+
 {
-	int row1[3] = {1, 2, 3};
-	int row1[3] = {4, 5, 6};
-	int row1[3] = { 7, 8, 9 };
+	
+	displayBoard();
 
 
 
