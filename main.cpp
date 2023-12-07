10494b7ea67e73717f2a0e904ad4ec3568dc5848
sahil <sahil.98@hotmail.com>
2023-12-07 23:29:23 +0000

    Created outer and inner for loop to output 2d array as a tic tac toe board.

--- main.cpp
+++ main.cpp
@@ -24,6 +24,13 @@ void displayBoard2() {
 			{' ', ' ', ' '}
 	};
 
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout << "||" << array[i][j];
+		}
+		cout << endl;
+	}
+
 }
 
 int main()
