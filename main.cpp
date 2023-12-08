d77840a9ce1cdb2c18470b6c19db933c42df0e19
sahil <sahil.98@hotmail.com>
2023-12-08 11:41:41 +0000

    following: Modfied displayboard with additional spacing, instructions func with endl and playerMove with char playerSymbol to plot x or o

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ char playerSymbol;
 void displayBoard(char array[3][3]) {
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
-			cout << "||" << array[i][j];
+			cout <<"  " << "|" << array[i][j];
 		}
 		cout << endl;
 	}
@@ -16,10 +16,10 @@ void instructions() {
 	cout << "Welcome to tic toe land" << endl;
 	cout << "The board is a 3x3 grid and each cell represented by its row and column number" << endl;
 	cout << "To make a move, enter the row and column where you want to place symbol" << endl;
-	cout << "Hint: Place you symbol in the left corner of the board by entering '0 0' ";
+	cout << "Hint: Place you symbol in the left corner of the board by entering '0 0'" << endl;
 }
 
-bool playerMove(char setArray [3][3], int row, int col) {
+bool playerMove(char setArray [3][3], int row, int col, char playerSymbol) {
 	if (setArray[row][col] == ' ') {
 		setArray[row][col] = playerSymbol;
 		return true;
