a5a436e23a42bcd2e73f36a2740c09cf9ceea5c8
sahil <sahil.98@hotmail.com>
2023-12-08 09:43:49 +0000

    removed previous displayBoard method and refactored 2d array func to 'displayBoard'

--- main.cpp
+++ main.cpp
@@ -2,21 +2,7 @@
 using namespace std;
 
 
-void displayBoard(){
-
-	int row1[3] = {1, 2, 3};
-	int row2[3] = {4, 5, 6};
-	int row3[3] = { 7, 8, 9 };
-
-	// int array[3][3]
-
-	cout << "\n\t" << row1[0] << "|" << row1[1] << "|" << row1[2] << "|";
-	cout << "\n\t" << row2[0] << "|" << row2[1] << "|" << row2[2] << "|";
-	cout << "\n\t" << row3[0] << "|" << row3[1] << "|" << row3[2] << "|"<<endl;
-}
-
-
-void displayBoard2() {
+void displayBoard() {
 
 	char array[3][3] = {
 			{' ', ' ', ' '},
@@ -37,7 +23,6 @@ int main()
 
 {	
 	displayBoard();
-	displayBoard2();
 
 
 
