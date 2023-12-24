e984aa3731ce4c517dd41bca9ec52f91dae26286
sahil <sahil.98@hotmail.com>
2023-12-24 17:19:57 +0000

    project has been converted to OOP and some additional methods have been applied for the logic of the game.

--- main.cpp
+++ main.cpp
@@ -1,65 +1,107 @@
 #include <iostream>
 using namespace std;
 
-char player = 'X';
-char computer = 'O';
+
 char playerSymbol;
-void displayBoard(char setArray[9]) {
-	for (int i = 0; i < 9; i++) {
-		cout << setArray[i] << " | ";
-		if ((i + 1) % 3 == 0) {
-			// if reached the final column, begin a new row
-			cout << endl;
+
+class TicTacToe {
+private:
+	char array[9];
+	char currentPlayer;
+	char opponent;
+	// char computer;
+public:
+	TicTacToe() : array{ ' ', ' ', ' ',' ', ' ',' ',' ', ' ', ' ' } {
+		currentPlayer = askPlayer("Do you want to go first? (y/n)") == 'y' ? 'X' : 'O';
+		updateOpponent();
+	}
+
+	void SwitchPlayer() {
+		if (currentPlayer == 'X') {
+			currentPlayer = 'O';
+		}
+		else {
+			currentPlayer = 'x';
 		}
 	}
-}
 
-void instructions() {
-	cout << "Welcome to tic toe land" << endl;
-	cout << "The board is a 3x3 grid and each cell represented by its row number" << endl;
-	cout << "To make a move, enter the row number based on the board to place symbol" << endl;
-	cout << "Hint: Pick any numbers between 1 - 9 " << endl;
-}
+	void instructions() {
+		cout << "Welcome to tic toe land" << endl;
+		cout << "The board is a 3x3 grid and each cell represented by its row number" << endl;
+		cout << "To make a move, enter the row number based on the board to place symbol" << endl;
+		cout << "Hint: Pick any numbers between 1 - 9 " << endl;
+	}
 
-void guide(char setArray[9]) {
-	for (int i = 0; i < 9; i++) {
-		cout << i + 1 << " | " << setArray[i];
-		if ((i + 1) % 3 == 0) {
-			cout << endl;
+	void guide(char setArray[9]) {
+		for (int i = 0; i < 9; i++) {
+			cout << i + 1 << " | " << setArray[i];
+			if ((i + 1) % 3 == 0) {
+				cout << endl;
+			}
 		}
 	}
-}
 
-bool playerMove(char setArray[9]) {
-	int position;
-	cout << "Enter a number based on board position " << endl;
-	// this is probably the biggest changes
-	// !(cin >> position) will ask the question as part of the loop and it must have an answer
-	// to save on checking all positions, just validated numbers below or above 1
-	// cin.peek() will check the next character after any numbers to confirm it is a new line, if it is a . for example for a decimal, it will register invalid
-	// position != static_cast<int>(position) just makes sure it is a number
-	while (!(cin >> position) || position < 1 || position > 9 || cin.peek() != '\n' || position != static_cast<int>(position))
-	{
-		cout << "Invalid, position has been taken"<<endl;
-		cin.clear();
-		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	void displayBoard(char setArray[9]) {
+		for (int i = 0; i < 9; i++) {
+			cout << setArray[i] << " | ";
+			if ((i + 1) % 3 == 0) {
+				// if reached the final column, begin a new row
+				cout << endl;
+			}
+		}
 	}
-	setArray[position - 1] = player;
-	return true;
+	bool playerMove(char setArray[9]) {
+		int position;
+		cout << "Enter a number based on board position " << endl;
+		// this is probably the biggest changes
+		// !(cin >> position) will ask the question as part of the loop and it must have an answer
+		// to save on checking all positions, just validated numbers below or above 1
+		// cin.peek() will check the next character after any numbers to confirm it is a new line, if it is a . for example for a decimal, it will register invalid
+		// position != static_cast<int>(position) just makes sure it is a number
+		while (!(cin >> position) || position < 1 || position > 9 || cin.peek() != '\n' || position != static_cast<int>(position))
+		{
+			cout << "Invalid, position has been taken" << endl;
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+		setArray[position - 1] = currentPlayer;
+		return true;
 
-} 
+	}
 
-char askPlayer(string question) {
-	char response;
-	do {
-		cout << question;
-		cin >> response;
-	} while (response != 'y' && response != 'n');
+	bool computerMove(char setArray[9]) {
+		int position;
+		srand(time(0));
 
-	return response;
-}
+		while (true) {
+			position = rand() % 9;
+			if (setArray[position] == ' ') {
+				setArray[position] = currentPlayer;
+				return true;
+				break;
+			}
+		}
 
+	}
 
+private:
+	char askPlayer(string question) {
+		char response;
+		do {
+			cout << question;
+			cin >> response;
+		} while (response != 'y' && response != 'n');
+
+		return response;
+	}
+	void updateOpponent() {
+		opponent = (currentPlayer== 'X') ? 'O' : 'X';
+	}
+
+	 
+
+};
+/*
 char playerPiece() {
 	char go_first = askPlayer("Do you want to go first? (y / no) ");
 	if (go_first == 'y') {
@@ -80,32 +122,14 @@ char opponentPiece(char symbol) {
 		return player;
 	}
 }
-
-
-//TOD0: Issue with input of 'n and player unable to switch to '0'.
-//TOD0: When player plots a poistion of the computer it replaces it. Need to fix
-bool computerMove(char setArray[9]) {
-	int position;
-	srand(time(0));
-
-	while (true) {
-		position = rand() % 9;
-		if (setArray[position] == ' ') {
-			setArray[position] = opponentPiece(player);
-			return true;
-			break;
-		}
-	}
-
-}
+*/
 int main()
-{	
+{
 	char array[9] = {
 			' ', ' ', ' ',
 			' ', ' ',' ',
 			' ', ' ', ' '
 	};
-	instructions();
 	guide(array);
 	char human = playerPiece();
 	char computer = opponentPiece(human);
